88ffccd72882d3a62d62d41f286adb620aff921c
AgungNugroho17 <112893441+AgungNugroho17@users.noreply.github.com>
2022-09-05 23:43:41 +0700

    Add files via upload

---
+++ tugas 1 daspro/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama           : Agung Nugroho" << endl;
+    cout << "NIM            : A11.2022.14125" << endl;
+    cout << "Alamat         : Kota Semarang" << endl;
+    cout << "Asal Sekolah   : SMA N 2 Mranggen Demak" << endl;
+    return 0;
+}
