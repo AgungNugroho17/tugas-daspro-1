7291084fafe7a368285fc65d5b64616923503740
AgungNugroho17 <112893441+AgungNugroho17@users.noreply.github.com>
2022-09-14 20:37:35 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+    cout << "Hello world!" << endl;
+    cout << "Kenalan yuk" << endl;
+    cout << "Nama kamu siapa ?";
+    cin >> nama;
+
+    cout << "Hi " << nama;
+    printf (" Kamu Harus Tau !!!");
+
+
+    int x = 19;
+    float t = 165.5;
+    char b = 'L';
+
+    cout << "\nUmur Saya " << x;
+    cout << "\nTinggi Badan " << t;
+    cout << "\nUkuran Baju " << b;
+
+    return 0;
+}
