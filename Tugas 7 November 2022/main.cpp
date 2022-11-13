9922b50c61060eed155c7f4b78076702fe63fbcd
AgungNugroho17 <112893441+AgungNugroho17@users.noreply.github.com>
2022-11-13 19:15:07 +0700

    Add files via upload

---
+++ Tugas 7 November 2022/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int uang = 5000;
+    int permen;
+
+    cin >> x ;
+
+    if(x==500){
+        permen = 5;
+        cout << permen << endl;
+    }
+    else if (x==1000){
+        permen = uang/1000;
+        cout << permen << endl;
+
+}
+
+{
+
+for (int x = 300; x< uang ; x+=300){
+        cout << x << endl;
+
+}
+
+        }
+
+
+
+
+
+    return 0;
+    }
+
